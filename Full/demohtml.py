9ad9b04e0f1cfece993fc520a7cca6ad87a4ac32
Olearndev <mis.olivier@gmail.com>
2025-02-01 20:37:47 -0500

    Install panda for files

--- Full/demohtml.py
+++ Full/demohtml.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,6 +11,24 @@ def index():
 def filters():
     sometext = 'Text to filter'
     return render_template('filters.html', some_text=sometext )
- 
+
+@app.template_filter()
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s * times
+
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('filters'))
+
+
 if __name__ == '__main__':                  
     app.run(host='0.0.0.0', debug=True)
